3d679baf0d133b0dd7a9a4c78aabc3687e5e2382
taradaszje <j.sularz@gmail.com>
2019-11-29 14:52:46 +0100

    This stable version for docker deployment

--- credit/src/main/java/com/example/credit/CreditApplication.java
+++ credit/src/main/java/com/example/credit/CreditApplication.java
@@ -2,11 +2,7 @@ package com.example.credit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients("com.example.credit")
 @SpringBootApplication
