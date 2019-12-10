e1f42e1857b9c4a274b80f3d07a4de54b5ad08ab
Jaroslaw Sularz <jarek.sularz@gmail.com>
2019-12-10 12:24:46 +0100

    Fix maven dependecies

--- netflix-eureka-naming-server/src/test/java/com/taradaszje/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
+++ netflix-eureka-naming-server/src/test/java/com/taradaszje/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -1,13 +1,13 @@
 package com.taradaszje.netflixeurekanamingserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NetflixEurekaNamingServerApplicationTests {
+public class NetflixEurekaNamingServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
